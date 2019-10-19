3dd6f30aeece4f620d1b7dde49c008d4dc6043e9
Dima Krutolianov <DimaKrutDK@ya.ru>
2019-10-19 17:51:42 +0300

    chore: convert onChange value to Formik format
    Formik doesn't support the way, that react-datetime pass value
    So we need to change this

--- src/components/labelled-date-input/LabelledDateInput.jsx
+++ src/components/labelled-date-input/LabelledDateInput.jsx
@@ -4,8 +4,13 @@ import 'react-datetime/css/react-datetime.css'
 import 'moment/locale/ru'
 import './LabelledDateInput.css'
 import LabelledInput from '../labelled-input'
+import PropTypes from 'prop-types'
 
-const LabelledDateInput = (props) => (
+const convertReactDateTimeValueToFormikFormat = (name, value) => ({
+  target: { name, value },
+})
+
+const LabelledDateInput = ({ name, ...props }) => (
   <LabelledInput
     labelClassName="labelled-date-input__label"
     renderInput={(inputProps) => (
@@ -14,10 +19,25 @@ const LabelledDateInput = (props) => (
         inputProps={{ className: 'labelled-date-input__input' }}
         locale="ru"
         {...inputProps}
+        onChange={(value) =>
+          inputProps.onChange(
+            convertReactDateTimeValueToFormikFormat(name, value)
+          )
+        }
       />
     )}
     {...props}
   />
 )
+
+LabelledDateInput.propTypes = {
+  name: PropTypes.string.isRequired,
+  // Formik name
+  onChange: PropTypes.func,
+}
+
+LabelledDateInput.defaultProps = {
+  onChange: () => {},
+}
 
 export default LabelledDateInput
