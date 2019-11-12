864e334933c6004ccc9e1ddbbecad33cef19c8c1
Dima Krutolianov <DimaKrutDK@ya.ru>
2019-11-12 20:59:10 +0300

    feat: "create-order" api

--- packages/backend/create-order_handler.js
+++ packages/backend/create-order_handler.js
@@ -1,6 +1,14 @@
+const { getMongoClient } = require('./utils')
 const { createLambda } = require('./middlewares')
 
-module.exports.handler = createLambda(async (event) => ({
-  message: 'Hello world!',
-  input: event,
-}))
+module.exports.handler = createLambda(async (event) => {
+  const orderData = event.body
+  const mongoClient = await getMongoClient()
+  await mongoClient
+    .db()
+    .collection('orders')
+    .insertOne(orderData)
+  return {
+    id: orderData.id,
+  }
+})
