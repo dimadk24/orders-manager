e1e79f6fbbfc631d8d4d934e6a8c449ea30e2c40
Dima Krutolianov <DimaKrutDK@ya.ru>
2019-11-23 19:12:40 +0300

    refactor: extract findOrders function

---
+++ packages/backend/src/orders-logic.js
@@ -0,0 +1,36 @@
+const QueryUtils = require('./query-utils')
+const { getMongoClient } = require('./utils')
+
+async function findOrders(filters) {
+  const client = await getMongoClient()
+  const queryFilters = []
+  if (filters.id)
+    queryFilters.push({
+      id: filters.id,
+    })
+  if (filters.phone) {
+    queryFilters.push({
+      $or: [
+        QueryUtils.includes('mainPhone', filters.phone),
+        QueryUtils.includes('additionalPhone', filters.phone),
+      ],
+    })
+  }
+  if (filters.city) queryFilters.push(QueryUtils.includes('city', filters.city))
+  if (filters.street)
+    queryFilters.push(QueryUtils.includes('street', filters.street))
+  if (filters.house)
+    queryFilters.push(QueryUtils.includes('house', filters.house))
+  return client
+    .db()
+    .collection('orders')
+    .find(
+      {
+        $and: queryFilters,
+      },
+      { limit: 50 }
+    )
+    .toArray()
+}
+
+module.exports = { findOrders }
--- packages/backend/src/search-orders_handler.js
+++ packages/backend/src/search-orders_handler.js
@@ -1,38 +1,8 @@
 const { createLambda } = require('./middlewares')
-const { getMongoClient } = require('./utils')
-const QueryUtils = require('./query-utils')
+const { findOrders } = require('./orders-logic')
 
 const handler = createLambda(async (event) => {
-  const { filters } = event.body
-  const client = await getMongoClient()
-  const queryFilters = []
-  if (filters.id)
-    queryFilters.push({
-      id: filters.id,
-    })
-  if (filters.phone) {
-    queryFilters.push({
-      $or: [
-        QueryUtils.includes('mainPhone', filters.phone),
-        QueryUtils.includes('additionalPhone', filters.phone),
-      ],
-    })
-  }
-  if (filters.city) queryFilters.push(QueryUtils.includes('city', filters.city))
-  if (filters.street)
-    queryFilters.push(QueryUtils.includes('street', filters.street))
-  if (filters.house)
-    queryFilters.push(QueryUtils.includes('house', filters.house))
-  const orders = await client
-    .db()
-    .collection('orders')
-    .find(
-      {
-        $and: queryFilters,
-      },
-      { limit: 50 }
-    )
-    .toArray()
+  const orders = await findOrders(event.body.filters)
   return { orders }
 })
 
