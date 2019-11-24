17560bd3ef7ca432b293d0d2fed29d21016d1d38
Dima Krutolianov <DimaKrutDK@ya.ru>
2019-11-24 11:05:21 +0300

    test: add tests for searcher

---
+++ packages/backend/src/orders-logic.test.js
@@ -0,0 +1,174 @@
+const { getMongoClient } = require('./utils')
+const { findOrders } = require('./orders-logic')
+
+describe('orders-logic', () => {
+  let client
+
+  const insertOrders = async (orders) => {
+    await client
+      .db()
+      .collection('orders')
+      .insertMany(orders)
+  }
+
+  beforeAll(async () => {
+    client = await getMongoClient()
+  })
+
+  afterEach(async () => {
+    await client.db().dropDatabase()
+  })
+
+  describe('findOrders', () => {
+    it('finds orders by id', async () => {
+      await insertOrders([
+        {
+          id: 1,
+          value: 'first',
+        },
+        {
+          id: 2,
+          value: 'second',
+        },
+        {
+          id: 11,
+        },
+      ])
+
+      const foundItems = await findOrders({ id: 1 })
+
+      expect(foundItems).toHaveLength(1)
+      expect(foundItems[0].id).toEqual(1)
+      expect(foundItems[0].value).toEqual('first')
+    })
+
+    it('finds orders by city case insensitively', async () => {
+      await insertOrders([
+        {
+          city: 'Minsk',
+        },
+        {
+          city: 'minsk main',
+        },
+        {
+          city: 'main Minsk',
+        },
+        {
+          city: 'Gomel',
+        },
+        {
+          city: 'Mensk',
+        },
+        {
+          id: 1,
+        },
+        {
+          city: '',
+        },
+        {
+          city: undefined,
+        },
+      ])
+
+      const foundItems = await findOrders({ city: 'Minsk' })
+
+      expect(foundItems).toHaveLength(3)
+      expect(foundItems[0]).toEqual(expect.objectContaining({ city: 'Minsk' }))
+      expect(foundItems[1]).toEqual(
+        expect.objectContaining({ city: 'minsk main' })
+      )
+      expect(foundItems[2]).toEqual(
+        expect.objectContaining({ city: 'main Minsk' })
+      )
+    })
+
+    it('find orders by street case insensitively', async () => {
+      await insertOrders([
+        {
+          address: { street: 'Nemiga' },
+        },
+        {
+          address: { street: 'nemiga str.' },
+        },
+        {
+          address: { street: 'str. Nemiga' },
+        },
+        {
+          id: 1,
+        },
+        {
+          address: { street: 'golodeda' },
+        },
+        {
+          address: { street: 'Nemigo' },
+        },
+      ])
+
+      const foundItems = await findOrders({ street: 'nemiga' })
+
+      expect(foundItems).toHaveLength(3)
+      expect(foundItems[0]).toEqual(
+        expect.objectContaining({ address: { street: 'Nemiga' } })
+      )
+      expect(foundItems[1]).toEqual(
+        expect.objectContaining({ address: { street: 'nemiga str.' } })
+      )
+      expect(foundItems[2]).toEqual(
+        expect.objectContaining({ address: { street: 'str. Nemiga' } })
+      )
+    })
+
+    it('finds orders by house', async () => {
+      await insertOrders([
+        {
+          address: {
+            house: '2',
+          },
+        },
+        {
+          address: {
+            house: '3',
+          },
+        },
+        {
+          address: {
+            house: '20',
+          },
+        },
+        {
+          id: 1,
+        },
+      ])
+
+      const foundItems = await findOrders({ house: '2' })
+
+      expect(foundItems).toHaveLength(1)
+      expect(foundItems[0]).toEqual(
+        expect.objectContaining({ address: { house: '2' } })
+      )
+    })
+
+    it('finds orders by id and street', async () => {
+      await insertOrders([
+        {
+          id: 1,
+          address: {
+            street: 'Nemiga',
+          },
+        },
+        {
+          id: 1,
+          address: {
+            street: 'Golodeda',
+          },
+        },
+        {
+          id: 10,
+          address: {
+            street: 'Nemiga',
+          },
+        },
+      ])
+    })
+  })
+})
